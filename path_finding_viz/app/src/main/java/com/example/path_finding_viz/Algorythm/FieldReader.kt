4ecafc565de5354e87c6de029c1fa60a3fe22b79
defrozentruth <metro2033dg@gmail.com>
2023-07-10 12:28:59 +0300

    rewrite in\out for new classes

--- path_finding_viz/app/src/main/java/com/example/path_finding_viz/Algorythm/FieldReader.kt
+++ path_finding_viz/app/src/main/java/com/example/path_finding_viz/Algorythm/FieldReader.kt
@@ -1,10 +1,15 @@
-import java.io.BufferedReader
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import com.example.path_finding_viz.CellType
+import com.example.path_finding_viz.ExtraPosition
 import java.io.File
-import java.io.FileReader
-import java.io.IOException
+import com.example.path_finding_viz.State
+
 
 class FieldReader() {
-    fun readField(filename: String):Field{
+    @Composable
+    fun readField(filename: String):State{
         val file = File(filename)
         val lines = file.readLines()
         val sizeX = lines[0].split(" ")[0].toInt()
@@ -13,18 +18,20 @@ class FieldReader() {
         val startY = lines[1].split(" ")[1].toInt()
         val finishX = lines[2].split(" ")[0].toInt()
         val finishY = lines[2].split(" ")[1].toInt()
-        val field = Field(sizeX, sizeY, startX, startY, finishX, finishY)
+        val startPos = ExtraPosition(remember {mutableStateOf(startX)}, remember {mutableStateOf(startY)})
+        val finPos = ExtraPosition(remember {mutableStateOf(finishX)}, remember {mutableStateOf(finishY)})
+        val field = State(sizeY, sizeX, startPos, finPos)
         var i = 0
         var j = 0
         for(k in 3 until lines.size){
             if(k % 2 != 0){
-                field.cells[i][j].up = lines[k].split(" ")[0].toInt()
-                field.cells[i][j].right = lines[k].split(" ")[1].toInt()
-                field.cells[i][j].down = lines[k].split(" ")[2].toInt()
-                field.cells[i][j].left = lines[k].split(" ")[3].toInt()
+                field.getCells()[i][j].uppJump = lines[k].split(" ")[0].toInt()
+                field.getCells()[i][j].rightJump = lines[k].split(" ")[1].toInt()
+                field.getCells()[i][j].downJump = lines[k].split(" ")[2].toInt()
+                field.getCells()[i][j].leftJump = lines[k].split(" ")[3].toInt()
             }else{
                 if(lines[k] == "Unpassable") {
-                    field.cells[i][j].accessibility = false
+                    field.getCells()[i][j].type = CellType.WALL
                 }
                 if(j == sizeX-1){
                     j = 0
--- path_finding_viz/app/src/main/java/com/example/path_finding_viz/Algorythm/FieldWriter.kt
+++ path_finding_viz/app/src/main/java/com/example/path_finding_viz/Algorythm/FieldWriter.kt
@@ -1,26 +1,29 @@
-import java.io.BufferedReader
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import com.example.path_finding_viz.CellType
+import com.example.path_finding_viz.ExtraPosition
 import java.io.File
-import java.io.FileReader
-import java.io.IOException
+import com.example.path_finding_viz.State
 
 class FieldWriter {
-    fun writeField(field:Field, filename: String){
+    fun writeField(field:State, filename: String){
         val file = File(filename)
         file.bufferedWriter().use { out ->
-            out.write("${field.sizeX} ${field.sizeY}\n")
-            out.write("${field.startX} ${field.startY}\n")
-            out.write("${field.finishX} ${field.finishY}\n")
-            for(i in 0 until field.sizeY)
-                for(j in 0 until field.sizeX){
-                    out.write("${field.cells[i][j].up} ${field.cells[i][j].left} ${field.cells[i][j].down} ${field.cells[i][j].right}\n")
-                    if(field.cells[i][j].accessibility){
-                        if(i == field.sizeY-1 && j == field.sizeX-1)
+            out.write("${field.width} ${field.height}\n")
+            out.write("${field.startPosition.column} ${field.startPosition.row}\n")
+            out.write("${field.finishPosition.column} ${field.finishPosition.row}\n")
+            for(i in 0 until field.height)
+                for(j in 0 until field.width){
+                    out.write("${field.getCells()[i][j].uppJump} ${field.getCells()[i][j].leftJump} ${field.getCells()[i][j].downJump} ${field.getCells()[i][j].rightJump}\n")
+                    if(field.getCells()[i][j].type != CellType.WALL){
+                        if(i == field.height-1 && j == field.width-1)
                             out.write("Normal")
                         else
                             out.write("Normal\n")
                     }
                     else{
-                        if(i == field.sizeY-1 && j == field.sizeX-1)
+                        if(i == field.height-1 && j == field.width-1)
                             out.write("Unpassable")
                         else
                             out.write("Unpassable\n")
